fd3b38e302230035711703672c00bd35a720ea46
Min Thu Wai <minthuwai.min@gmail.com>
2020-03-16 23:01:52 +0630

    add support for generating BuiltList, BuiltMap, and BuiltSet

--- lib/src/class_builder.dart
+++ lib/src/class_builder.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'dart:typed_data';
 
 import 'package:analyzer/dart/element/element.dart';
@@ -6,13 +7,17 @@ import 'package:hive/hive.dart';
 import 'package:hive_generator/src/builder.dart';
 import 'package:hive_generator/src/helper.dart';
 import 'package:source_gen/source_gen.dart';
-import 'package:dartx/dartx.dart';
+import 'package:built_collection/built_collection.dart';
 
 class ClassBuilder extends Builder {
   var hiveListChecker = const TypeChecker.fromRuntime(HiveList);
-  var listChecker = const TypeChecker.fromRuntime(List);
-  var mapChecker = const TypeChecker.fromRuntime(Map);
-  var setChecker = const TypeChecker.fromRuntime(Set);
+  // var listChecker = const TypeChecker.fromRuntime(List);
+  // var mapChecker = const TypeChecker.fromRuntime(Map);
+  // var setChecker = const TypeChecker.fromRuntime(Set);
+  // var iterableChecker = const TypeChecker.fromRuntime(Iterable);
+  var builtListChecker = const TypeChecker.fromRuntime(BuiltList);
+  var builtMapChecker = const TypeChecker.fromRuntime(BuiltMap);
+  var builtSetChecker = const TypeChecker.fromRuntime(BuiltSet);
   var iterableChecker = const TypeChecker.fromRuntime(Iterable);
   var uint8ListChecker = const TypeChecker.fromRuntime(Uint8List);
 
@@ -24,49 +29,26 @@ class ClassBuilder extends Builder {
   String buildRead() {
     var code = StringBuffer();
 
-    // TODO change (b) => b to empty
     code.writeln('''
     final numOfFields = reader.readByte();
     final fields = <int, dynamic>{
       for (var i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
-    return ${cls.name}((b) => b
+    return (${cls.name}Builder()
     ''');
 
-    var constr = cls.constructors.firstOrNullWhere((it) => it.name.isEmpty);
-    check(constr != null, 'Provide an unnamed constructor.');
-
-    // The remaining fields to initialize.
-    // TODO change getters to setters
-    var fields = getters.toList();
-
-    // TODO uncomment below codes to make this on par with original generator
-    // var initializingParams =
-    //     constr.parameters.where((param) => param.isInitializingFormal);
-    // for (var param in initializingParams) {
-    //   var field = fields.firstOrNullWhere((it) => it.name == param.name);
-    //   // Final fields
-    //   field ??= getters.firstOrNullWhere((it) => it.name == param.name);
-    //   if (field != null) {
-    //     if (param.isNamed) {
-    //       code.write('${param.name}: ');
-    //     }
-    //     code.writeln('${_cast(param.type, 'fields[${field.index}]')},');
-    //     fields.remove(field);
-    //   }
-    // }
-
-    // code.writeln(')');
-
-    // There may still be fields to initialize that were not in the constructor
-    // as initializing formals. We do so using cascades.
+    var fields = getters;
     for (var field in fields) {
-      code.writeln(
-          '..${field.name} = ${_cast(field.type, 'fields[${field.index}]')}');
+      // check(!isMapOrIterable(field.type),
+      //     'Please use BuiltList, BuiltSet or BuiltMap');
+
+      final type = field.type;
+      final index = field.index;
+
+      code.writeln('..${field.name} = ${_cast(type, 'fields[$index]')}');
     }
 
-    // TODO remove all string except ";"
-    code.writeln(');');
+    code.writeln(').build();');
 
     return code.toString();
   }
@@ -76,50 +58,56 @@ class ClassBuilder extends Builder {
       return '($variable as HiveList)?.castHiveList()';
     } else if (iterableChecker.isAssignableFromType(type) &&
         !isUint8List(type)) {
-      return '($variable as List)${_castIterable(type)}';
-    } else if (mapChecker.isExactlyType(type)) {
-      return '($variable as Map)${_castMap(type)}';
+      return '${_castIterable(type, variable)}';
+    } else if (builtMapChecker.isExactlyType(type)) {
+      return '${_castMap(type, variable)}';
     } else {
-      return '$variable as ${type.name}';
+      return '$variable as ${type.getDisplayString()}';
     }
   }
 
   bool isMapOrIterable(DartType type) {
-    return listChecker.isExactlyType(type) ||
-        setChecker.isExactlyType(type) ||
+    return builtListChecker.isExactlyType(type) ||
+        hiveListChecker.isExactlyType(type) ||
+        builtSetChecker.isExactlyType(type) ||
         iterableChecker.isExactlyType(type) ||
-        mapChecker.isExactlyType(type);
+        builtMapChecker.isExactlyType(type);
   }
 
   bool isUint8List(DartType type) {
     return uint8ListChecker.isExactlyType(type);
   }
 
-  String _castIterable(DartType type) {
+  String _castIterable(DartType type, String variable) {
     var paramType = type as ParameterizedType;
     var arg = paramType.typeArguments[0];
-    if (isMapOrIterable(arg) && !isUint8List(arg)) {
+    print('$arg ${isMapOrIterable(arg)} and ${isUint8List(type)}');
+    if (isMapOrIterable(type) && !isUint8List(arg)) {
       var cast = '';
-      if (listChecker.isExactlyType(type)) {
-        cast = '?.toList()';
-      } else if (setChecker.isExactlyType(type)) {
-        cast = '?.toSet()';
+      if (builtListChecker.isExactlyType(type)) {
+        cast = 'ListBuilder<$arg>($variable as List)';
+      } else if (builtSetChecker.isExactlyType(type)) {
+        cast = 'SetBuilder<$arg>($variable as Set)';
       }
-      return '?.map((dynamic e)=> ${_cast(arg, 'e')})$cast';
+      // return '?.map((dynamic e)=> ${_cast(arg, 'e')})$cast';
+      return cast;
     } else {
-      return '?.cast<${arg.name}>()';
+      return '($variable as List)?.cast<${arg.getDisplayString()}>()';
     }
   }
 
-  String _castMap(DartType type) {
+  String _castMap(DartType type, String variable) {
     var paramType = type as ParameterizedType;
     var arg1 = paramType.typeArguments[0];
     var arg2 = paramType.typeArguments[1];
     if (isMapOrIterable(arg1) || isMapOrIterable(arg2)) {
-      return '?.map((dynamic k, dynamic v)=>'
-          'MapEntry(${_cast(arg1, 'k')},${_cast(arg2, 'v')}))';
+      // return '?.map((dynamic k, dynamic v)=>'
+      //     'MapEntry(${_cast(arg1, 'k')},${_cast(arg2, 'v')}))';
+      return 'MapBuilder<$arg1, $arg2>($variable as Map)';
     } else {
-      return '?.cast<${arg1.name}, ${arg2.name}>()';
+      return '''($variable as Map)?
+      .cast<${arg1.getDisplayString()}, ${arg2.getDisplayString()}>()
+      ''';
     }
   }
 
@@ -140,7 +128,8 @@ class ClassBuilder extends Builder {
   }
 
   String _convertIterable(DartType type, String accessor) {
-    if (setChecker.isExactlyType(type) || iterableChecker.isExactlyType(type)) {
+    if (builtSetChecker.isExactlyType(type) ||
+        iterableChecker.isExactlyType(type)) {
       return '$accessor?.toList()';
     } else {
       return accessor;
